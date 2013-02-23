e5be34dc26ed92d1390819bd057c47130bde9119
Jason Mitchell <jason.mitchell.w@gmail.com>
2013-02-23 15:23:01 -0500

    More refactoring..

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,9 +11,9 @@ namespace OpenSprinkler.Test
             var app = new SprinklerDriver(5);
             app.Start(new[] { 0, 1, 2, 3, 4 });
 
-            Thread.Sleep(1000);
+            //Thread.Sleep(10000);
 
-            app.Stop(); // wait 10 seconds and shut em all off
+            //app.Stop(); // wait 10 seconds and shut em all off
 
             Console.ReadLine();
         }
