c5c46366b57bd082671c3a04003344148fe36a45
Jason Mitchell <jason.mitchell.w@gmail.com>
2013-02-23 13:19:53 -0500

    Refactoring

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -2,18 +2,16 @@
 using System.Threading;
 using OpenSprinkler.Manager;
 
-namespace OpenSprinkler.Driver
+namespace OpenSprinkler.Test
 {
     class Program
     {
         static void Main(string[] args)
         {
             var app = new SprinklerDriver(5);
-            app.Start(new int[] { 0, 1, 2, 3, 4 });
+            app.Start(new[] { 0, 1, 2, 3, 4 });
 
-            app.Start(new int[] { 0, 1, 2, 3, 4 });
-
-            Thread.Sleep(30000);
+            Thread.Sleep(1000);
 
             app.Stop(); // wait 10 seconds and shut em all off
 
